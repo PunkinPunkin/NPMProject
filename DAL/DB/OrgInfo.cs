60f7009cc9c56dde50af60690f010baebb9ba5e8
josh_chen002 <matt91057@gmail.com>
2020-08-13 16:26:48 +0800

    first push

---
+++ DAL/DB/OrgInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.DB
+{
+    public class OrgInfo
+    {
+        [Key]
+        public int OrgId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string OrgName { get; set; }
+
+        [MaxLength(100)]
+        public string OrgAddress { get; set; }
+
+    }
+}
---
+++ DAL/NPMContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DAL.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class NPMContext : DbContext
+    {
+        private string _connectionString;
+        public NPMContext() : base() { }
+        public NPMContext(string connectionString) : base() { _connectionString = connectionString; }
+        public NPMContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<OrgInfo> OrgInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                if (string.IsNullOrEmpty(_connectionString))
+                {
+                    _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=;Persist Security Info=False;User ID=josh;Password=5a45nF9uW6";
+                }
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+    }
+
+}
